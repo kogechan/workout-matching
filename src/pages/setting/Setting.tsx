700ecb0763cbabfa22e00cb8341bf8cb38f0acbf
jun0612 <june061212@icloud.com>
2025-04-17 15:13:46 +0900

    各種設定ページ作成

---
+++ src/pages/setting/Setting.tsx
@@ -0,0 +1,147 @@
+// pages/settings.tsx
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import {
+  Box,
+  Container,
+  Typography,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Paper,
+} from '@mui/material';
+import {
+  Info as InfoIcon,
+  Gavel as GavelIcon,
+  PrivacyTip as PrivacyTipIcon,
+  Notifications as NotificationsIcon,
+  Logout as LogoutIcon,
+  Delete as DeleteIcon,
+} from '@mui/icons-material';
+import { useAtom } from 'jotai';
+import { logoutModalAtom } from '@/jotai/Jotai';
+
+export const Setting = () => {
+  const router = useRouter();
+  const [notificationsEnabled, setNotificationsEnabled] = useState(true);
+  const [, setLogoutModalOpen] = useAtom(logoutModalAtom);
+  const [deleteAccountDialogOpen, setDeleteAccountDialogOpen] = useState(false);
+
+  const handleNotificationChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNotificationsEnabled(event.target.checked);
+  };
+
+  return (
+    <Container maxWidth="md">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom align="center">
+          各種設定
+        </Typography>
+
+        <Paper elevation={3} sx={{ mt: 4, p: 3 }}>
+          <List>
+            {/* サービスについて */}
+            <ListItem onClick={() => router.push('/')}>
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
+              <ListItemText primary="サービスについて" />
+            </ListItem>
+
+            <Divider />
+
+            {/* 利用規約 */}
+            <ListItem>
+              <ListItemIcon>
+                <GavelIcon />
+              </ListItemIcon>
+              <ListItemText primary="利用規約" />
+            </ListItem>
+
+            <Divider />
+
+            {/* プライバシーポリシー */}
+            <ListItem>
+              <ListItemIcon>
+                <PrivacyTipIcon />
+              </ListItemIcon>
+              <ListItemText primary="プライバシーポリシー" />
+            </ListItem>
+
+            <Divider />
+
+            {/* 通知設定 */}
+            <ListItem>
+              <ListItemIcon>
+                <NotificationsIcon />
+              </ListItemIcon>
+              <ListItemText primary="通知設定" />
+              <Switch
+                edge="end"
+                checked={notificationsEnabled}
+                onChange={handleNotificationChange}
+                inputProps={{
+                  'aria-labelledby': 'switch-notifications',
+                }}
+              />
+            </ListItem>
+
+            <Divider />
+
+            {/* ログアウト */}
+            <ListItem onClick={() => setLogoutModalOpen(true)}>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary="アカウントからログアウト" />
+            </ListItem>
+
+            <Divider />
+
+            {/* 退会 */}
+            <ListItem onClick={() => setDeleteAccountDialogOpen(true)}>
+              <ListItemIcon>
+                <DeleteIcon />
+              </ListItemIcon>
+              <ListItemText primary="退会" />
+            </ListItem>
+          </List>
+        </Paper>
+      </Box>
+
+      {/* 退会確認ダイアログ */}
+      <Dialog
+        open={deleteAccountDialogOpen}
+        onClose={() => setDeleteAccountDialogOpen(false)}
+        aria-labelledby="delete-account-dialog-title"
+        aria-describedby="delete-account-dialog-description"
+      >
+        <DialogTitle id="delete-account-dialog-title">退会の確認</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="delete-account-dialog-description">
+            退会するとすべてのデータが削除され、元に戻すことはできません。本当に退会しますか？
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDeleteAccountDialogOpen(false)}>
+            キャンセル
+          </Button>
+          <Button color="error" autoFocus>
+            退会する
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Container>
+  );
+};
--- src/pages/setting/index.tsx
+++ src/pages/setting/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import { Setting } from './Setting';
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +9,7 @@ const Home: NextPage = () => {
         <title>各種設定</title>
         <meta name="description" content="各種設定" />
       </Head>
-      <h1>設定</h1>
+      <Setting />
     </>
   );
 };
