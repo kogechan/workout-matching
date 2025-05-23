e98dd1a7ce84d1a004d74870bbf00b181f72f40d
jun0612 <june061212@icloud.com>
2025-05-23 18:22:13 +0900

    BlockListに変更

---
+++ src/components/block/BlockList.tsx
@@ -0,0 +1,3 @@
+export const BlockList = () => {
+  return <></>;
+};
---
+++ src/pages/block/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import { BlockList } from '@/components/block/BlockList';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <BlockList />
+    </>
+  );
+};
+
+export default Home;
